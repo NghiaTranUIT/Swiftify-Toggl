1c5adf8437dbd018ffa8d83ac90a7a052cfdbef1
Nghia Tran <vinhnghiatran@gmail.com>
2018-10-15 20:10:56 +0700

    Initial Commit

---
+++ Desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Desktop
+//
+//  Created by Nghia Tran on 10/15/18.
+//  Copyright © 2018 com.nsproxy.proxy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
